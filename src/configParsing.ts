11652c99ceddbe14dd7c10c5e83ebc161241ee0f
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-27 01:06:27 -0400

    docs: add docs to `configParsing.ts`

--- src/configParsing.ts
+++ src/configParsing.ts
@@ -1,3 +1,10 @@
+/**
+ * This file contains the logic for parsing the config file.
+ * The config file is a JSON file that contains the configuration for the app.
+ * The config file is validated against a JSON schema using the `zod` library.
+ * The `AppConfig` class is a singleton class that loads the config file and provides global access to the config data.
+ */
+
 import { promises as fs } from "fs";
 import { Config } from "./types";
 import { z } from "zod";
@@ -30,12 +37,19 @@ const ConfigSchema = z.object({
   webserver: WebserverConfigSchema,
 });
 
+/**
+ * Represents the application configuration.
+ */
 export class AppConfig {
   private static instance: AppConfig;
   private config: Config | null = null;
 
   private constructor() {}
 
+  /**
+   * Returns the singleton instance of AppConfig.
+   * @returns The singleton instance of AppConfig.
+   */
   public static getInstance(): AppConfig {
     if (!AppConfig.instance) {
       AppConfig.instance = new AppConfig();
@@ -43,6 +57,12 @@ export class AppConfig {
     return AppConfig.instance;
   }
 
+  /**
+   * Loads the configuration from the specified file path.
+   * @param configPath - The path to the configuration file.
+   * @returns A promise that resolves when the configuration is loaded.
+   * @throws If there is an error parsing the config file.
+   */
   public async loadConfig(configPath: string): Promise<void> {
     if (this.config === null) {
       try {
@@ -56,6 +76,11 @@ export class AppConfig {
     }
   }
 
+  /**
+   * Returns the loaded configuration.
+   * @returns The loaded configuration.
+   * @throws If the configuration has not been loaded yet.
+   */
   public getConfig(): Config {
     if (this.config === null) {
       throw new Error("Config has not been loaded yet.");
