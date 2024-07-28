dbc6d883d818053552a1f4dfedc90792ddf2b51d
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-28 02:10:26 -0400

    feat: implement Discord message sending
    using 3 strategies, top to bottom:
    if webhooks are enabled and id + token are provided, they are sent using webhooks
    
    then, if emoji id + name are provided, they are sent as regular messages with an emoji prepended to them
    
    otherwise just a regular text message is sent

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { authenticateTwitch } from "./twitch/twitchOauthServer.js";
 import { AppConfig } from "./AppConfig.js";
 import { Twitch } from "./twitch/Twitch.js";
 import { registerTwitchMessageHandler } from "./twitch/messageHandling.js";
+import { DiscordClient } from "./discord/DiscordClient.js";
+import { Webhook } from "./discord/Webhook.js";
 
 const CONFIG_FILE_PATH = "./config.json";
 export const TOKEN_DATA_PATH = "./tokens.json";
@@ -22,6 +24,12 @@ const startApp = async () => {
 
     // Register the Twitch message handler
     await registerTwitchMessageHandler();
+
+    // Init Discord
+    const discord = await DiscordClient.getInstance();
+
+    // Init webhook
+    const webhook = Webhook.getInstance();
   } catch (error) {
     console.error("Failed to start the app due to config error:", error);
   }
--- src/twitch/messageHandling.ts
+++ src/twitch/messageHandling.ts
@@ -1,6 +1,8 @@
 import { ChatMessage } from "@twurple/chat";
 import { Twitch } from "./Twitch.js";
-import { TwitchMessage, TwitchUser } from "../types.js";
+import { DiscordMessageStrategy, TwitchMessage, TwitchUser } from "../types.js";
+import { Webhook } from "../discord/Webhook.js";
+import { DiscordClient } from "../discord/DiscordClient.js";
 
 export const registerTwitchMessageHandler = async () => {
   const twitch = await Twitch.getInstance();
@@ -16,7 +18,7 @@ export const registerTwitchMessageHandler = async () => {
       };
       twitch.cacheMessage(message);
 
-      // Cache user
+      // Cache user if not already cached
       const userIsCached = twitch.getUser(user);
       if (!userIsCached) {
         await twitch.clients.apiClient?.users
@@ -35,6 +37,42 @@ export const registerTwitchMessageHandler = async () => {
             console.error(`Error getting user: ${error}`);
           });
       }
+
+      sendMessageToDiscord(user, text, twitch.getUser(user)?.profilePictureUrl);
     }
   );
+};
+
+const sendMessageToDiscord = async (
+  username: string,
+  message: string,
+  profilePictureUrl?: string
+) => {
+  // Send message to Discord using the selected strategy
+  const strategy = (await DiscordClient.getInstance()).getMessageStrategy();
+
+  if (strategy === DiscordMessageStrategy.Webhook) {
+    sendWebhookMessage(username, message, profilePictureUrl);
+  } else if (strategy === DiscordMessageStrategy.Emoji) {
+    sendEmojiMessage(username, message);
+  } else {
+    sendRegularMessage(username, message);
+  }
+};
+
+// Discord message sending strategies
+const sendWebhookMessage = async (
+  username: string,
+  message: string,
+  profilePictureUrl?: string
+) => {
+  Webhook.sendMessage(username, message, profilePictureUrl);
+};
+
+const sendEmojiMessage = async (username: string, message: string) => {
+  DiscordClient.sendMessage(username, message);
+};
+
+const sendRegularMessage = async (username: string, message: string) => {
+  DiscordClient.sendMessage(username, message);
 };
