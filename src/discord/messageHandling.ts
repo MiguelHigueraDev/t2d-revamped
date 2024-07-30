f976dcb1e9b91c5303098e6f170ec14b7d4ecb98
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-30 00:30:42 -0400

    feat: implement message linking in both message handlers

--- src/discord/messageHandling.ts
+++ src/discord/messageHandling.ts
@@ -2,23 +2,60 @@ import { Events } from "discord.js";
 import { DiscordClient } from "./DiscordClient.js";
 import { AppConfig } from "../AppConfig.js";
 import { Twitch } from "../twitch/Twitch.js";
+import { LinkedCache } from "../linking/LinkedCache.js";
 
 export const registerDiscordMessageHandler = async () => {
-  const discordClient = (await DiscordClient.getInstance()).getClient();
+  const discord = await DiscordClient.getInstance();
   const config = AppConfig.getInstance().getConfig().discord;
   const twitch = await Twitch.getInstance();
 
-  discordClient.on(Events.MessageCreate, async (message) => {
-    if (message.author.bot) {
-      return;
+  discord.getClient().on(Events.MessageCreate, async (message) => {
+    const {
+      id: messageId,
+      channelId,
+      content: text,
+      author: { id: userId, username },
+    } = message;
+
+    // Cache message
+    discord.cacheMessage({
+      id: messageId,
+      channelId,
+      text,
+      userId,
+    });
+
+    // Don't send bot messages
+    if (!message.author.bot) {
+      if (message.channelId === config.channelId) {
+        await twitch.sendMessage(username, text);
+      }
     }
 
-    if (message.channelId === config.channelId) {
-      const username = message.author.username;
-      const messageText = message.content;
-      twitch.sendMessage(username, messageText);
+    // After sending the message to Twitch, try to find the matching Twitch message
+    // and link it with the Discord message
+    const matchingTwitchMessage = await findMatchingTwitchMessage(
+      username,
+      text
+    );
+
+    if (matchingTwitchMessage) {
+      LinkedCache.getInstance().linkDiscordMessageToTwitchMessage(
+        messageId,
+        matchingTwitchMessage.id
+      );
     }
+
+    console.table(LinkedCache.getInstance().getCachedMessages());
   });
 
   console.log("Discord message handler registered.");
+};
+
+// Finds the matching Twitch message in the cache to link it with the Discord message
+const findMatchingTwitchMessage = async (username: string, message: string) => {
+  const twitchMessages = (await Twitch.getInstance()).getCachedMessages();
+  return twitchMessages.find((msg) => {
+    return msg.text === message && msg.user === username;
+  });
 };
--- src/twitch/messageHandling.ts
+++ src/twitch/messageHandling.ts
@@ -1,9 +1,10 @@
 import { ChatMessage } from "@twurple/chat";
 import { Twitch } from "./Twitch.js";
-import { DiscordMessageStrategy, TwitchMessage, TwitchUser } from "../types.js";
+import { DiscordMessageStrategy } from "../types.js";
 import { Webhook } from "../discord/Webhook.js";
 import { DiscordClient } from "../discord/DiscordClient.js";
 import { AppConfig } from "../AppConfig.js";
+import { LinkedCache } from "../linking/LinkedCache.js";
 
 export const registerTwitchMessageHandler = async () => {
   const twitch = await Twitch.getInstance();
@@ -11,45 +12,74 @@ export const registerTwitchMessageHandler = async () => {
 
   twitch.clients.unauthenticatedChatClient?.onMessage(
     async (channel: string, user: string, text: string, msg: ChatMessage) => {
-      // Ignore messages from the Twitch "bot"
-      if (user === twitchUsername) {
-        return;
-      }
+      // Extract only message from string [D] user: message when the bot sends a message
+      const extractedMessage = text.match(/:\s*(.*)/);
+      let finalMessage = text;
+      if (extractedMessage) finalMessage = extractedMessage[1];
 
-      // Cache message
-      const message: TwitchMessage = {
+      // Cache all messages, including the ones sent by the bot
+      twitch.cacheMessage({
         id: msg.id,
         channel,
         user,
-        text,
-      };
-      twitch.cacheMessage(message);
+        text: finalMessage,
+      });
+
+      // Also cache Twitch part in mixed cache (only here because if done in Discord
+      // side it would be done twice)
+      LinkedCache.getInstance().cacheTwitchMessage(msg.id);
 
       // Cache user if not already cached
-      const userIsCached = twitch.getUser(user);
-      if (!userIsCached) {
-        await twitch.clients.apiClient?.users
-          .getUserByName(user)
-          .then((foundUser) => {
-            if (foundUser) {
-              const user: TwitchUser = {
-                username: foundUser.name,
-                displayName: foundUser.displayName,
-                profilePictureUrl: foundUser.profilePictureUrl,
-              };
-              twitch.cacheUser(user);
-            }
-          })
-          .catch((error) => {
-            console.error(`Error getting user: ${error}`);
-          });
+      await cacheUser(twitch, user);
+
+      // Don't send bot messages
+      if (user !== twitchUsername) {
+        sendMessageToDiscord(
+          user,
+          text,
+          twitch.getUser(user)?.profilePictureUrl
+        );
       }
 
-      sendMessageToDiscord(user, text, twitch.getUser(user)?.profilePictureUrl);
+      // After sending the message to Discord, try to find the matching Discord message
+      // and link it with the Twitch message
+      const matchingDiscordMessage = await findMatchingDiscordMessage(
+        user,
+        finalMessage
+      );
+
+      if (matchingDiscordMessage) {
+        LinkedCache.getInstance().linkTwitchMessageToDiscordMessage(
+          msg.id,
+          matchingDiscordMessage.id
+        );
+      }
     }
   );
 
   console.log("Twitch message handler registered.");
+};
+
+const cacheUser = async (twitch: Twitch, user: string): Promise<void> => {
+  const userIsCached = twitch.getUser(user);
+  if (userIsCached) {
+    return;
+  }
+
+  await twitch.clients.apiClient?.users
+    .getUserByName(user)
+    .then((foundUser) => {
+      if (foundUser) {
+        twitch.cacheUser({
+          username: foundUser.name,
+          displayName: foundUser.displayName,
+          profilePictureUrl: foundUser.profilePictureUrl,
+        });
+      }
+    })
+    .catch((error) => {
+      console.error(`Error getting user: ${error}`);
+    });
 };
 
 const sendMessageToDiscord = async (
@@ -57,19 +87,28 @@ const sendMessageToDiscord = async (
   message: string,
   profilePictureUrl?: string
 ) => {
-  // Send message to Discord using the selected strategy
   const strategy = (await DiscordClient.getInstance()).getMessageStrategy();
 
   if (strategy === DiscordMessageStrategy.Webhook) {
     sendWebhookMessage(username, message, profilePictureUrl);
-  } else if (strategy === DiscordMessageStrategy.Emoji) {
-    sendEmojiMessage(username, message);
   } else {
     sendRegularMessage(username, message);
   }
 };
 
+const findMatchingDiscordMessage = async (user: string, text: string) => {
+  const discordMessages = (
+    await DiscordClient.getInstance()
+  ).getCachedMessages();
+  return discordMessages.find((msg) => {
+    // TODO: Check if user is the same
+    return msg.text === text;
+  });
+};
+
 // Discord message sending strategies
+
+// Sends a message to Discord using a webhook that has the user's username and profile picture
 const sendWebhookMessage = async (
   username: string,
   message: string,
@@ -78,10 +117,7 @@ const sendWebhookMessage = async (
   Webhook.sendMessage(username, message, profilePictureUrl);
 };
 
-const sendEmojiMessage = async (username: string, message: string) => {
-  DiscordClient.sendMessage(username, message);
-};
-
+// Sends a regular or emoji message to Discord (depending on the configuration) with the bot's username
 const sendRegularMessage = async (username: string, message: string) => {
   DiscordClient.sendMessage(username, message);
 };
